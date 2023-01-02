a26ca1051809dcfd392f4db4204170cd90b9897e
Djamel Dahmane <d.dahmane@criteo.com>
2023-01-02 19:43:55 +0100

    first release of lib1
    Change-Id: Ib7c9811d17e8af54351b7ea6771019ea26253f2b

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/djdhm/lib1
+
+go 1.19
+
+require github.com/djdhm/external1 v1.0.0
---
+++ lib/mylib.go
@@ -0,0 +1,8 @@
+package lib1
+
+import  "fmt"
+import  "github.com/djdhm/external1/lib"
+func testing() {
+   fmt.Println("I am in lib1 using external lib")
+   display()
+}
