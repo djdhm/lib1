fa9943db0e48e7b47fb9788404017d42909726c5
Djamel Dahmane <d.dahmane@criteo.com>
2023-01-03 17:04:35 +0100

    Bump external version to 1.9.1
    Change-Id: Ica2cf1dc4c3e4864d2cc3294fb0c7664ae367235

--- lib/mylib.go
+++ lib/mylib.go
@@ -3,6 +3,6 @@ package lib
 import  "fmt"
 import  "github.com/djdhm/external1/lib"
 func Testing() {
-   fmt.Println("I am lib1 version: 1.4.0 and using external1 v1.9.0")
+   fmt.Println("I am lib1 version: 1.5.0 and using external1 v1.9.1")
    display.Display()
 }
