684d89e73438141478723d9a9303d510c188d64c
Djamel Dahmane <d.dahmane@criteo.com>
2023-01-03 16:08:03 +0100

    Creating another version
    Change-Id: I2c683a4af2e777922afd355db364f83928e889c1

--- lib/mylib.go
+++ lib/mylib.go
@@ -3,6 +3,6 @@ package lib
 import  "fmt"
 import  "github.com/djdhm/external1/lib"
 func Testing() {
-   fmt.Println("I am in lib1 using external lib")
+   fmt.Println("I am in lib1 using external lib In 1.2.0")
    display.Display()
 }
