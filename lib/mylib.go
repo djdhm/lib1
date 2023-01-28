4fdc4268f455a824f0c0de348307533243861cad
Djamel Dahmane <d.dahmane@criteo.com>
2023-01-28 20:43:52 +0100

    Second release of my lib1
     - Bump external dep to v1.1.0
    
    Change-Id: Ic42098dcb275a1b51305b4b4be21f85f9b06ab24

--- lib/mylib.go
+++ lib/mylib.go
@@ -1,8 +1,12 @@
-package lib1
+package lib
 
-import  "fmt"
-import  "github.com/djdhm/external1/lib"
-func testing() {
-   fmt.Println("I am in lib1 using external lib")
-   display()
+import (
+	"fmt"
+
+	display "github.com/djdhm/external1/lib"
+)
+
+func Testing() {
+	fmt.Println("I am lib1 version: 1.0.1 and using external1 v1.1.0")
+	display.Display()
 }
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+import "github.com/djdhm/lib1/lib"
+
+func main() {
+	fmt.Println("vim-go")
+        lib.Testing()
+}
